fec525eb8d5db3221561c665100215cafbe3168c
AliAais <aaisali228@gmail.com>
2025-02-23 17:32:37 +0500

    fix

--- src/models/OTP.ts
+++ src/models/OTP.ts
@@ -1,19 +1,28 @@
-import mongoose from "mongoose";
+import mongoose, { Schema, Document } from "mongoose";
+
+// User Info to Store Temporarily with OTP
+const TempUserSchema = new Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true },
+  password: { type: String, required: true },
+});
 
 export interface IOTP extends Document {
   email: string;
   otp: string;
+  tempUser: {
+    name: string;
+    email: string;
+    password: string;
+  };
   expiresAt: Date;
 }
 
-const OTPSchema = new mongoose.Schema({
+const OTPSchema = new Schema<IOTP>({
   email: { type: String, required: true },
   otp: { type: String, required: true },
-  expiresAt: {
-    type: Date,
-    required: true,
-    default: () => new Date(Date.now() + 10 * 60 * 1000),
-  }, // 10 mins expiry
+  tempUser: { type: TempUserSchema, required: true },
+  expiresAt: { type: Date, required: true },
 });
 
-export const OTP = mongoose.model("Otp", OTPSchema);
+export const OTP = mongoose.model<IOTP>("OTP", OTPSchema);
